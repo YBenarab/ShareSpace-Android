744827b3a8e4aede712fa64b986a50b9544fa27e
Youssef Benarab <ybe@capasystems.com>
2026-03-29 15:52:42 +0200

    Add Spring Boot backend project

---
+++ backend/sharespace-backend/src/main/java/com/sharespace/sharespacebackend/SharespaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sharespace.sharespacebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharespaceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharespaceBackendApplication.class, args);
+    }
+
+}
---
+++ backend/sharespace-backend/src/test/java/com/sharespace/sharespacebackend/SharespaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharespace.sharespacebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharespaceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
